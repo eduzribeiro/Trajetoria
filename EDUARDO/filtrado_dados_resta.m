8b2bd30a394bfa065d1e8693c657cb16ac580dac
Eduardo <eduzribeiro@gmail.com>
2016-07-29 15:40:04 -0300

    Últimos resultados anteriores a mudança na integral

--- EDUARDO/filtrado_dados_resta.m
+++ EDUARDO/filtrado_dados_resta.m
@@ -1,8 +1,8 @@
-function [x y z]=filtrado_dados_resta(a_n,L)
+function [x y z]=filtrado_dados_resta(a_n,inicio,L)
 
-    x=mean(a_n(1:L,1));
-    y=mean(a_n(1:L,2));
-    z=mean(a_n(1:L,3));
+    x=mean(a_n(inicio:inicio+L-1,1));
+    y=mean(a_n(inicio:inicio+L-1,2));
+    z=mean(a_n(inicio:inicio+L-1,3));
     
 %     dx = std(a_n(1:L,1));
 %     dy = std(a_n(1:L,2));
